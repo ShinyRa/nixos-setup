917402c0ff0f6252a0f23b350208720ed7a4e1bc
tijs <tijs.degraaff16@gmail.com>
2024-06-24 13:36:44 +0200

    feat: Beta nividia driver package

--- nixos/system/nvidia.nix
+++ nixos/system/nvidia.nix
@@ -7,13 +7,15 @@
 
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "nvidia";
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_DESKTOP = "Hyprland";
     XDG_SESSION_TYPE = "wayland";
     GBM_BACKEND = "nvidia-drm";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
   };
   
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
